c9c30299586c0ceaab152b98262740fe59fa2bad
Ali Badara <abdougouba1@gmail.com>
2024-11-12 16:39:56 +0100

    Add NumberToWordsController implementation

--- src/main/java/com/wemanity/numbersinwords/controller/NumberToWordsController.java
+++ src/main/java/com/wemanity/numbersinwords/controller/NumberToWordsController.java
@@ -1,7 +1,27 @@
 package com.wemanity.numbersinwords.controller;
+import com.wemanity.numbersinwords.service.NumberToWordsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/numbers")
 public class NumberToWordsController {
+
+    private final NumberToWordsService numberToWordsService;
+
+    @Autowired
+    public NumberToWordsController(NumberToWordsService numberToWordsService) {
+        this.numberToWordsService = numberToWordsService;
+    }
+
+    @GetMapping("/to-words/{number}")
+    public String convertNumberToWords(@PathVariable int number) {
+        return numberToWordsService.convertNumberToWords(number);
+    }
+
+    @GetMapping("/to-number")
+    public int convertWordsToNumber(@RequestParam String words) {
+        return numberToWordsService.convertWordsToNumber(words);
+    }
 
 }
