d400c63bc9aa76ea96cab9cb98aef73a24257d06
Ali Badara <abdougouba1@gmail.com>
2024-11-12 14:33:27 +0100

    Create tree arrays of strings UNITS, TENS, THOUSANDS

--- src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
+++ src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
@@ -5,6 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class NumberToWordsServiceImpl implements NumberToWordsService {
 
+    private static final String[] UNITS = {
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    };
+
+    private static final String[] TENS = {
+            "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    };
+
+    private static final String[] THOUSANDS = {
+            "", "thousand", "million", "billion"
+    };
+
     @Override
     public String convertNumberToWords(int number) {
         return "";
