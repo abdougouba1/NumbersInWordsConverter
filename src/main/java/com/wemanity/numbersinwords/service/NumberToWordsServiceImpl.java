d196eea145550098ce6eaf346f7cf92aad7b7a69
Ali Badara <abdougouba1@gmail.com>
2024-11-12 14:25:43 +0100

    Implement Service Interface

---
+++ src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
@@ -0,0 +1,17 @@
+package com.wemanity.numbersinwords.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NumberToWordsServiceImpl implements NumberToWordsService {
+
+    @Override
+    public String convertNumberToWords(int number) {
+        return "";
+    }
+
+    @Override
+    public int convertWordsToNumber(String words) {
+        return 0;
+    }
+}
