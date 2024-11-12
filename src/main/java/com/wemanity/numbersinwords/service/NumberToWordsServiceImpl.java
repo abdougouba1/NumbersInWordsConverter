c06ab4c69eb5920b007350d270bc023c67390b24
Ali Badara <abdougouba1@gmail.com>
2024-11-12 15:44:25 +0100

    Update convertNumberToWords method implement

--- src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
+++ src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
@@ -1,43 +1,10 @@
 package com.wemanity.numbersinwords.service;
 
+import com.wemanity.numbersinwords.util.NumberToWordsUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NumberToWordsServiceImpl implements NumberToWordsService {
-
-    private static final String[] UNITS = {
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-    };
-
-    private static final String[] TENS = {
-            "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
-    };
-
-    private static final String[] THOUSANDS = {
-            "", "thousand", "million", "billion"
-    };
-
-    private String convertLessThanThousand(int number) {
-        String result;
-
-        if (number % 100 < 20) {
-            result = UNITS[number % 100];
-            number /= 100;
-        } else {
-            result = UNITS[number % 10];
-            number /= 10;
-
-            result = TENS[number % 10] + " " + result;
-            number /= 10;
-        }
-
-        if (number == 0) {
-            return result;
-        }
-
-        return UNITS[number] + " hundred" + " " + result;
-    }
 
     @Override
     public String convertNumberToWords(int number) {
@@ -50,7 +17,8 @@ public class NumberToWordsServiceImpl implements NumberToWordsService {
 
         while (number > 0) {
             if (number % 1000 != 0) {
-                words.insert(0, convertLessThanThousand(number % 1000) + THOUSANDS[index] + " ");
+                words.insert(0, NumberToWordsUtil.convertLessThanThousand(number % 1000)
+                        + " " + NumberToWordsUtil.THOUSANDS[index] + " ");
             }
             number /= 1000;
             index++;
