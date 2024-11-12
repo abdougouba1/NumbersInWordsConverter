9fdc3afbc075f8ab0565da1b2f7fa0126d42eca2
Ali Badara <abdougouba1@gmail.com>
2024-11-12 14:38:49 +0100

    Create convertLessThanThousand method

--- src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
+++ src/main/java/com/wemanity/numbersinwords/service/NumberToWordsServiceImpl.java
@@ -18,6 +18,27 @@ public class NumberToWordsServiceImpl implements NumberToWordsService {
             "", "thousand", "million", "billion"
     };
 
+    private String convertLessThanThousand(int number) {
+        String result;
+
+        if (number % 100 < 20) {
+            result = UNITS[number % 100];
+            number /= 100;
+        } else {
+            result = UNITS[number % 10];
+            number /= 10;
+
+            result = TENS[number % 10] + " " + result;
+            number /= 10;
+        }
+
+        if (number == 0) {
+            return result;
+        }
+
+        return UNITS[number] + " hundred" + " " + result;
+    }
+
     @Override
     public String convertNumberToWords(int number) {
         return "";
